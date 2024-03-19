06520512cdce9e05132f98e980265a3c068fc5d8
Perryjay30 <pelumijsh@gmail.com>
2024-03-19 12:32:57 +0100

    Introducing Provider

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:fooderlich_v2/screens/explore_screen.dart';
 import 'package:fooderlich_v2/screens/recipes_screen.dart';
 import 'screens/grocery_screen.dart';
+import 'package:provider/provider.dart';
+import 'models/models.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,7 +13,7 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
-  int _selectedIndex = 0;
+  // int _selectedIndex = 0;
 
   static List<Widget> pages = <Widget>[
     const ExploreScreen(),
@@ -19,41 +21,64 @@ class HomeState extends State<Home> {
     const GroceryScreen(),
   ];
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+  // void _onItemTapped(int index) {
+  //   setState(() {
+  //     _selectedIndex = index;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Fooderlich',
-          style: Theme.of(context).textTheme.headline6),
-        centerTitle: true,
-      ),
-      body: pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.explore),
-            label: 'Explore',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.book),
-            label: 'Recipes',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: 'To Buy',
-          ),
-        ],
-      ),
-    );
+    return Consumer<TabManager>(builder: (context, tabManager,
+        child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Fooderlich',
+              style: Theme.of(context).textTheme.headline6),),
+        body: pages[tabManager.selectedTab],
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Theme.of(context)
+              .textSelectionTheme.selectionColor,
+          currentIndex: tabManager.selectedTab,
+          onTap: (index) {
+            tabManager.goToTab(index);
+          },
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.explore), label: 'Explore',),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.book), label: 'Recipes',),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list), label: 'To Buy',),
+          ],),);
+    },);
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text(
+    //       'Fooderlich',
+    //       style: Theme.of(context).textTheme.headline6),
+    //     centerTitle: true,
+    //   ),
+    //   body: pages[_selectedIndex],
+    //   bottomNavigationBar: BottomNavigationBar(
+    //     selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+    //     currentIndex: _selectedIndex,
+    //     onTap: _onItemTapped,
+    //     items: const <BottomNavigationBarItem>[
+    //       BottomNavigationBarItem(
+    //         icon: Icon(Icons.explore),
+    //         label: 'Explore',
+    //       ),
+    //       BottomNavigationBarItem(
+    //         icon: Icon(Icons.book),
+    //         label: 'Recipes',
+    //       ),
+    //       BottomNavigationBarItem(
+    //         icon: Icon(Icons.list),
+    //         label: 'To Buy',
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
--- lib/screens/empty_grocery_screen.dart
+++ lib/screens/empty_grocery_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/models.dart';
 
 
 class EmptyGroceryScreen extends StatelessWidget {
@@ -7,7 +9,37 @@ class EmptyGroceryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO 3: Replace and add layout widgets
-    return Container(color: Colors.purple);
+    return Padding(
+        padding: EdgeInsets.all(30.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AspectRatio(
+                aspectRatio: 1 / 1,
+                child: Image.asset('assets/fooderlich_assets/empty_list.png'),
+                ),
+              const SizedBox(height: 8.0),
+              const Text('No Groceries', style: TextStyle(fontSize: 21.0),),
+              const SizedBox(height: 16.0),
+              const Text(
+                'Shopping for ingredients?\n'
+                    'Tap the + button to write them down!',
+                textAlign: TextAlign.center,
+              ),
+              MaterialButton(
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),),
+                color: Colors.green,
+                onPressed: () {
+                  Provider.of<TabManager>(context, listen: false).goToRecipes();
+                },
+                child: const Text('Browse Recipes'),
+              ),
+            ],
+          ),
+        ),
+    );
   }
 }
